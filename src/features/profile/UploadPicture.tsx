5e0116196a301e3bd9d716a65612c9bdd40514ef
QiZhang-UConn <Qi.Zhang@uconn.edu>
2022-01-06 15:40:22 -0500

    upload picture UI ready

---
+++ src/features/profile/UploadPicture.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { reverbClientUploadFileWithAuth } from "../../remote/reverb-api/reverbClient";
+
+export default function Upload_Picture() {
+  const [firstName, setFirstName] = React.useState("");
+  const [cjsFile, setcjsFile] = React.useState("");
+  const [cjsFile2, setcjsFile2] = React.useState("");
+  const submitForm = (event:any) => {
+    event.preventDefault();
+    const dataArray = new FormData();
+    dataArray.append('file', cjsFile);
+    dataArray.append('file2', cjsFile2);
+    dataArray.append("firstName", firstName);
+
+    try{
+      reverbClientUploadFileWithAuth.post("/storage/uploadfile",dataArray);
+      console.log("Upload finished")
+    }
+    catch{
+      console.log("upload failed")
+    }
+  };
+  const handleChangeFile = (event:any)=> {
+    const file = event.target.files[0];
+    setcjsFile(file);
+  }
+  const handleChangeFile2 = (event:any)=> {
+    const file2 = event.target.files[0];
+    setcjsFile2(file2);
+  }
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+      <label htmlFor="last_name">Add/Change Profile Picture</label>
+        <div className="form_input">
+          <input type="file" onChange={(e) => handleChangeFile(e)} />
+        </div>
+      <label htmlFor="last_name">Add/Change Header Picture</label>
+        <div className="form_input">
+          <input type="file" onChange={(e) => handleChangeFile2(e)} />
+        </div>
+        <input type="submit"/>
+      </form>
+    </div>
+  );
+}
--- src/remote/reverb-api/reverbClient.ts
+++ src/remote/reverb-api/reverbClient.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { store } from '../../app/store'
 
 const reverbClient = axios.create( {
-// baseURL: 'http://Reverbapiapplication-env.eba-qjeevjhg.us-west-1.elasticbeanstalk.com',
-  baseURL: 'http://localhost:5000',
+// baseURL: 'http://reverbapplication-env.eba-e5qubfd5.us-west-1.elasticbeanstalk.com/',
+  baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json'
   },
@@ -11,14 +11,23 @@ const reverbClient = axios.create( {
 } );
 
 const reverbClientWithAuth = axios.create( {
-// baseURL: 'http://Reverbapiapplication-env.eba-qjeevjhg.us-west-1.elasticbeanstalk.com',
-  baseURL: 'http://localhost:5000',
+// baseURL: 'http://reverbapplication-env.eba-e5qubfd5.us-west-1.elasticbeanstalk.com/',
+  baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
     'Authorization': ""
   },
   withCredentials: true,
 } );
+
+const reverbClientUploadFileWithAuth = axios.create( {
+    baseURL: 'http://localhost:8080',
+    headers: {
+      "Content-Type": "multipart/form-data; boundary=<calculated when request is sent>",
+      'Authorization': ""
+    },
+    withCredentials: true,
+  } );
 
 reverbClientWithAuth.interceptors.request.use( function ( config: any )
 {
@@ -27,4 +36,11 @@ reverbClientWithAuth.interceptors.request.use( function ( config: any )
   return config;
 } );
 
-export { reverbClient, reverbClientWithAuth };
+reverbClientUploadFileWithAuth.interceptors.request.use( function ( config: any )
+{
+  const token = "" + store.getState().auth[0].token;
+  config.headers["Authorization"] = token;
+  return config;
+} );
+
+export { reverbClient, reverbClientWithAuth , reverbClientUploadFileWithAuth};
